3f4755850ec87d768bd3661ba3accf27aec6f8ee
Peter Lawless <peterlawless223@gmail.com>
2016-10-09 08:00:29 -0400

    Got a rudimentary rotor index selection functionality to work using simple jQuery

--- enigma_app/static/js/main.js
+++ enigma_app/static/js/main.js
@@ -3,4 +3,35 @@ var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
                 'Y', 'Z'];
 
-console.log(alphabet);
+var slow_index = 0;
+var middle_index = 0;
+var fast_index = 0;
+
+$('.up').click(function () {
+  if ($(this).parent().parent().hasClass('slow-rotor')) {
+    slow_index = (slow_index + 1) % 26;
+    $(this).parent().prev().html(alphabet[slow_index]);
+  } else if ($(this).parent().parent().hasClass('middle-rotor')) {
+    middle_index = (middle_index + 1) % 26;
+    $(this).parent().prev().html(alphabet[middle_index]);
+  } else if ($(this).parent().parent().hasClass('fast-rotor')) {
+    fast_index = (fast_index + 1) % 26;
+    $(this).parent().prev().html(alphabet[fast_index]);
+  }
+});
+
+$('.down').click(function () {
+  if ($(this).parent().parent().hasClass('slow-rotor')) {
+    if (slow_index === 0) {slow_index = slow_index + 26;}
+    slow_index = (slow_index - 1) % 26;
+    $(this).parent().prev().html(alphabet[slow_index]);
+  } else if ($(this).parent().parent().hasClass('middle-rotor')) {
+    if (middle_index === 0) {middle_index = middle_index + 26;}
+    middle_index = (middle_index - 1) % 26;
+    $(this).parent().prev().html(alphabet[middle_index]);
+  } else if ($(this).parent().parent().hasClass('fast-rotor')) {
+    if (fast_index === 0) {fast_index = fast_index + 26;}
+    fast_index = (fast_index - 1) % 26;
+    $(this).parent().prev().html(alphabet[fast_index]);
+  }
+});
