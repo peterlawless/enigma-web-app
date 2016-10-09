1f21a04650cde2840afb447891fdc4c529b2e9be
Peter Lawless <peterlawless223@gmail.com>
2016-10-09 08:10:42 -0400

    Made adjustments to JavaScript file

--- enigma_app/static/js/main.js
+++ enigma_app/static/js/main.js
@@ -7,16 +7,18 @@ var slow_index = 0;
 var middle_index = 0;
 var fast_index = 0;
 
+$('.window').html('<span>' + alphabet[0] + '</span>');
+
 $('.up').click(function () {
   if ($(this).parent().parent().hasClass('slow-rotor')) {
     slow_index = (slow_index + 1) % 26;
-    $(this).parent().prev().html(alphabet[slow_index]);
+    $(this).parent().prev().html('<span>' + alphabet[slow_index] + '</span>');
   } else if ($(this).parent().parent().hasClass('middle-rotor')) {
     middle_index = (middle_index + 1) % 26;
-    $(this).parent().prev().html(alphabet[middle_index]);
+    $(this).parent().prev().html('<span>' + alphabet[middle_index] + '</span>');
   } else if ($(this).parent().parent().hasClass('fast-rotor')) {
     fast_index = (fast_index + 1) % 26;
-    $(this).parent().prev().html(alphabet[fast_index]);
+    $(this).parent().prev().html('<span>' + alphabet[fast_index] + '</span>');
   }
 });
 
@@ -24,14 +26,14 @@ $('.down').click(function () {
   if ($(this).parent().parent().hasClass('slow-rotor')) {
     if (slow_index === 0) {slow_index = slow_index + 26;}
     slow_index = (slow_index - 1) % 26;
-    $(this).parent().prev().html(alphabet[slow_index]);
+    $(this).parent().prev().html('<span>' + alphabet[slow_index] + '</span>');
   } else if ($(this).parent().parent().hasClass('middle-rotor')) {
     if (middle_index === 0) {middle_index = middle_index + 26;}
     middle_index = (middle_index - 1) % 26;
-    $(this).parent().prev().html(alphabet[middle_index]);
+    $(this).parent().prev().html('<span>' + alphabet[middle_index] + '</span>');
   } else if ($(this).parent().parent().hasClass('fast-rotor')) {
     if (fast_index === 0) {fast_index = fast_index + 26;}
     fast_index = (fast_index - 1) % 26;
-    $(this).parent().prev().html(alphabet[fast_index]);
+    $(this).parent().prev().html('<span>' + alphabet[fast_index] + '</span>');
   }
 });
