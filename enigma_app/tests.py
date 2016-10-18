b7e0ec961481949132698d81f4fa5d2ce57ca150
Peter Lawless <peterlawless223@gmail.com>
2016-10-18 12:12:37 -0400

    Started writing unit tests

--- enigma_app/tests.py
+++ enigma_app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from .views import encrypt, home, enigma
+
 
 # Create your tests here.
+class SmokeTest(TestCase):
+
+    def test_root_resolves_to_home(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    def test_slash_enigma_resolves_to_enigma(self):
+        found = resolve('/enigma')
+        self.assertEqual(found.func, enigma)
+
+    def test_slash_encrypt_resolves_to_encrypt(self):
+        found = resolve('/encrypt')
+        self.assertEqual(found.func, encrypt)
